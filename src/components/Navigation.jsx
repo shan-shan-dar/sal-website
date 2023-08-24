4605c6535baf3c6af0cee86a227fb383c6ef73ff
shan-shan-dar <105597557+shan-shan-dar@users.noreply.github.com>
2023-08-24 13:24:05 +0530

    hero, stats and outreach sections added

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,90 +1,22 @@
-import logo_full from '/logo_full.png'
-import logo_short from '/logo_short.png'
-import {motion} from 'framer-motion'
-import { useState } from 'react'
-import { useMediaQuery } from '../util/useMediaQuery'
-
 export default function Nav() {
-    const [toggle, setToggled] = useState(false)
-    const xl = useMediaQuery('(min-width: 1280px)')
-
-    //return the navbar
-    return (
-        <div className="bg-light rounded-xl shadow-md m-2 md:m-4 lg:m-10">
-            <nav className="relative mx-16 mb-24 flex justify-between items-center py-12 font-medium">
-
-                {xl ? (<div>
-                    <span className='text-5xl font-black text-accent'>Save a Life<br/>Initiative</span>
-                </div>) : (<div>
-                    <span className='text-6xl font-black text-accent'>SaL</span>
-                </div>)}
-                
-                
-                {xl && (
-                    <div className='flex gap-12 text-lg'>
-                        <a href="/">
-                            Home
-                        </a>
-                        <a href="/about">
-                            About
-                        </a>
-                        <a href="/outreach">
-                            Outreach
-                        </a>
-                        <a href="/app">
-                            IAP CPR App
-                        </a>
-                        <a href="/validation">
-                            Validation
-                        </a>
-                        <a href="/team">
-                            The Team
-                        </a>
-                        <a href="/contact">
-                            Contact Us
-                        </a>
-                    </div>
-                )}
-
-                {!xl && (
-                    <div className='space-y-2 cursor-pointer z-50' onClick={() => setToggled(prevToggle => !prevToggle)}>
-                        <motion.span className='block h-0.5 w-8 bg-dark'></motion.span>
-                        <motion.span className='block h-0.5 w-6 bg-dark'></motion.span>
-                        <motion.span className='block h-0.5 w-4 bg-dark'></motion.span>
-                    </div>  
-                )}
-
-                {toggle && !xl && (
-                    <motion.div
-                        animate={{opacity: 0.9, x: 0}}
-                        initial={{opacity: 0, x: 25}}
-                    className='fixed flex bg-lighter bottom-0 left-0 w-full h-screen items-center justify-center'>
-                        <div className='flex flex-col gap-16 text-lg'>
-                            <a href="/">
-                                Home
-                            </a>
-                            <a href="/about">
-                                About
-                            </a>
-                            <a href="/outreach">
-                                Outreach
-                            </a>
-                            <a href="/app">
-                                IAP CPR App
-                            </a>
-                            <a href="/validation">
-                                Validation
-                            </a>
-                            <a href="/team">
-                                The Team
-                            </a>
-                            <a href="/contact">
-                                Contact
-                            </a>
-                        </div>
-                    </motion.div>
-                )}
-            </nav>
+  //return the navbar
+  return (
+    <div className='bg-white'>
+      <nav className='relative mx-40 flex justify-between items-center py-6 font-normal'>
+        <div>
+          <span className='text-3xl font-black text-dartmouth'>
+            Save a Life
+          </span>
         </div>
-    )
+
+        <div className='flex gap-12 text-md'>
+          <a href='/'>Home</a>
+          <a href='/about'>About</a>
+          <a href='/outreach'>Outreach</a>
+          <a href='/app'>IAP CPR App</a>
+          <a href='/contact'>Contact</a>
+        </div>
+      </nav>
+    </div>
+  );
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,21 +1,28 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			fontFamily: {
-				poppins: ["Poppins", ...defaultTheme.fontFamily.sans],
-			},
-			colors: {
-				dark: "#252422",
-				light: "#FDF2B0",
-				lighter: "#FEFAE0",
-				accent: "#32936F",
-
-			},
-		},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        bitter: ["Bitter", "serif"],
+      },
+      colors: {
+        darkest: "#0d1e1c",
+        lightgreen: "#EDFCD9",
+        white: "#ffffff",
+        dartmouth: "#245650",
+        athena: "#A5D75F",
+      },
+      fontSize: {
+        "10xl": "8.5rem",
+      },
+      borderRadius: {
+        "4xl": "3rem",
+      },
+    },
+  },
+  plugins: [],
+};
