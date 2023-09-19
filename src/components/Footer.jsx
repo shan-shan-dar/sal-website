330f96cc2c7623a820ac0e9a2e00a4c827575995
shan-shan-dar <105597557+shan-shan-dar@users.noreply.github.com>
2023-09-19 13:30:37 +0530

    typo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,10 @@
 export default function Foot() {
   //return the navbar
   return (
-    <footer className="bg-white text-dartmouth py-10 flex justify-between px-40">
+    <footer className='bg-white text-dartmouth py-10 flex justify-between px-40'>
       <div>
-        <h4 className="text-2xl font-bold mb-4">Save a Life</h4>
-        <p className="text-base font-light">
+        <h4 className='text-2xl font-bold mb-4'>Save a Life</h4>
+        <p className='text-base font-light'>
           Sahyadri School KFI
           <br />
           Tiwai Hill, Rajgurunagar (Khed)
@@ -16,33 +16,32 @@ export default function Foot() {
       </div>
 
       <div>
-        <h4 className="text-lg font-medium mb-4">Contact</h4>
-        <div className="flex">
-          <p className="text-base font-light">initiative.savealife@gmail.com</p>
+        <h4 className='text-lg font-medium mb-4'>Contact</h4>
+        <div className='flex'>
+          <p className='text-base font-light'>initiative.savealife@gmail.com</p>
         </div>
-        <p className="text-base font-light">+91 9096590376</p>
+        <p className='text-base font-light'>+91 9096590376</p>
       </div>
 
-        <div className='flex flex-col items-center flex-1'>
-          <div>
-            <h4 className='text-lg font-medium mb-4'>Quick Links</h4>
-            <ul className='text-base font-light'>
-              <li>
-                <a href='#'>Home</a>
-              </li>
-              <li>
-                <a href='#'>Team</a>
-              </li>
-              <li>
-                <a href='#'>Outreach</a>
-              </li>
-              <li>
-                <a href='#'>IAP CPR App</a>
-              </li>
-            </ul>
-          </div>
+      <div className='flex flex-col items-center flex-1'>
+        <div>
+          <h4 className='text-lg font-medium mb-4'>Quick Links</h4>
+          <ul className='text-base font-light'>
+            <li>
+              <a href='#'>Home</a>
+            </li>
+            <li>
+              <a href='#'>Team</a>
+            </li>
+            <li>
+              <a href='#'>Outreach</a>
+            </li>
+            <li>
+              <a href='#'>IAP CPR App</a>
+            </li>
+          </ul>
         </div>
       </div>
     </footer>
-  )
+  );
 }
