126b54a2c258bf4fec7a3a574ac607edcd83d815
shan-shan-dar <105597557+shan-shan-dar@users.noreply.github.com>
2023-10-28 12:28:08 +0530

    photo changes

--- src/components/PersonCard.jsx
+++ src/components/PersonCard.jsx
@@ -1,14 +1,16 @@
 const PersonCard = ({ image, name, about }) => (
-  <div class='text-center'>
-    <img
-      src={image}
-      alt={name}
-      class='w-full aspect-square mx-auto rounded-4xl mb-7'
-    />
-    <h3 class='mx-autofont-bitter font-bold text-lightgreen text-4xl mb-2'>
+  <div className='text-center'>
+    <div className='bg-lightgreen w-full aspect-square mx-auto rounded-4xl mb-7'>
+      <img
+        src={image}
+        alt={name}
+        className='w-full h-full object-cover rounded-4xl'
+      />
+    </div>
+    <h3 className='mx-auto font-bitter font-bold text-lightgreen text-4xl mb-2'>
       {name}
     </h3>
-    <p class='text-athena font-medium text-2xl'>{about}</p>
+    <p className='text-athena font-medium text-2xl'>{about}</p>
   </div>
 );
 
